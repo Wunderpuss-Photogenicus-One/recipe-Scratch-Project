9126a0c737548e137056349bc561742a22f5205c
Jose Andrew <Jandrew205@outlook.com>
2024-12-17 17:35:19 -0500

    added components to frontend and css file structure

---
+++ src/client/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hero from './Hero.jsx';
+import IngredientList from './Ingredients.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <p>testing</p>
+      <Hero />
+      <IngredientList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/client/components/Ingredients.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IngredientList = () => {
+  return (
+    <div>
+      <h3>Ingredient List will be shown here</h3>
+    </div>
+  );
+};
+
+export default IngredientList;
