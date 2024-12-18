97c57cc49657ac3ab8761f8154ecb903acf81c93
Gilgamesh <gothicmonk138@gmail.com>
2024-12-18 15:11:50 -0500

    update controller

--- src/server/controllers/recipeController.js
+++ src/server/controllers/recipeController.js
@@ -4,4 +4,36 @@ const recipeController = {};
 
 recipeController.getIngredients = (req, res, next) => {
     
+    const getIngredients = 'select ingredient_id, ingredient_name, from ingredients';
+
+    db.query(getIngredients).then((result) => {
+        res.locals = result.rows;
+
+        return next()
+    })
+    .catch((err) => {
+        return next({
+            log: 'getIngredients middleware error' + err.message,
+            message: {err: 'Error occured in getIngredients'}
+
+        })
+    })
+};
+
+recipeController.getRecipe = (req, res, next) => {
+    
+    const getRecipe = 'select ingredient_id, ingredient_name, from ingredients';
+
+    db.query(getIngredients).then((result) => {
+        res.locals = result.rows;
+
+        return next()
+    })
+    .catch((err) => {
+        return next({
+            log: 'getIngredients middleware error' + err.message,
+            message: {err: 'Error occured in getIngredients'}
+
+        })
+    })
 }
