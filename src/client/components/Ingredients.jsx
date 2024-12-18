e33e05eeb51d0c899a200859e3bc13b33e20a844
Rachel Dean <rchldn@gmail.com>
2024-12-18 16:29:19 -0500

    ingredients component, just got handleChange working on individual checkboxes, still need to flesh out handleSubmit for button

--- src/client/components/Ingredients.jsx
+++ src/client/components/Ingredients.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
+import { useState } from 'react';
 
 const IngredientList = () => {
+  const fakeIngredients = ['apple', 'banana', 'milk'];
+  const [checked, setChecked] = useState({});
+
+  const handleChange = (event) => {
+    const { value, checked } = event.target;
+    console.log('I was checked!');
+    setChecked((previous) => ({
+      ...previous,
+      [value]: checked,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    console.log('formData: ', formData.entries());
+    console.log('formMethod: ', form.method);
+  };
+
+  console.log(fakeIngredients);
   return (
     <div>
       <h3>Ingredient List will be shown here</h3>
-      {/* then let's make the list into a checklist - for each ingredient, make a checkbox with an onChange prop that'll make h5 populate automatically */}
-      <div>
-      <Checkbox checked={unchecked} onChange={handleChange} />
-      </div>
+      <form method='post' onSubmit={handleSubmit}>
+        {fakeIngredients.map((element, index) => {
+          return (
+            <label key={index} htmlFor={element}>
+              {/* htmlFor is what we need in react to connect the label to the input */}
+              <input
+                id={index}
+                name='test'
+                type='checkbox'
+                checked={checked[element] || false}
+                onChange={handleChange}
+                value={element}
+              />
+              {element}
+            </label>
+          );
+        })}
+        <button type='submit'>Submit</button>
+      </form>
+
       <h5>
         This container might populate automatically as ingredients are selected
       </h5>
