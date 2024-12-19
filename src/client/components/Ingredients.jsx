72c82ed4232151bb825035c0077503725206d225
Jose Andrew <Jandrew205@outlook.com>
2024-12-19 15:09:56 -0500

    added list to the front end

--- src/client/components/Ingredients.jsx
+++ src/client/components/Ingredients.jsx
@@ -1,53 +1,77 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 
 const IngredientList = (props) => {
-  const fakeIngredients = ['apple', 'banana', 'milk'];
+  const { ingredientList } = props; //destructuring props object, grabbing the ingredient list fetched results
 
+  const fakeIngredients = ['apple', 'banana', 'milk']; // this will be deleted just a test
   const [checked, setChecked] = useState({});
-  // let array = [...props.ingredientList];
-  const { ingredientList } = props;
-  console.log('ingredientList:', ingredientList);
-
+  const [ingredientChosen, setIngredientChosen] = useState(new Set());
+  // console.log('ingredientList:', ingredientList); //just testing to see if ingredient list prints
+  let listOfIngredientsChosen = [...ingredientChosen];
+  console.log('listOfIngredientsChosen:', listOfIngredientsChosen);
+  //adds to the object 'checked' the boxes that are checked off
   const handleChange = (event) => {
-    const { value, checked } = event.target;
-    console.log('I was checked!');
-    console.log(event.target.value);
+    const { name, checked } = event.target;
+    console.log('name of item checked:', name);
+    console.log(checked);
+
     setChecked((previous) => ({
       ...previous,
-      [value]: checked,
+      [name]: checked,
     }));
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const form = e.target;
-    const formData = new FormData(form);
-    console.log('formData: ', formData.entries());
-    console.log('formMethod: ', form.method);
-  };
 
-  const handleClick = () => {
-    fetch('/api')
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => {
-        console.log('check fetch request for Recipe');
-      });
-  };
-
-  const handleIngredientListClick = () => {
     fetch('/api/recipes', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(fakeIngredients),
-    }).catch(() => {
-      (err) =>
-        console.error('Frontend to backend communication breakdown:', err);
-    });
+      body: JSON.stringify([...ingredientChosen]),
+    }).catch((err) =>
+      console.error('Frontend to backend communication breakdown:', err)
+    );
   };
+
+  //not needed anymore, for submit button, form has own submission event handler
+  // const handleClick = () => {
+  //   fetch('/api')
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data))
+  //     .catch((err) => {
+  //       console.log('check fetch request for Recipe');
+  //     });
+  // };
+
+  //this form is to be used onhandlesubmit
+  // const handleIngredientListClick = () => {
+  //   fetch('/api/recipes', {
+  //     method: 'POST',
+  //     headers: {
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(fakeIngredients),
+  //   }).catch(() => {
+  //     (err) =>
+  //       console.error('Frontend to backend communication breakdown:', err);
+  //   });
+  // };
+
+  // console.log(checked);
+  useEffect(() => {
+    const ingredientSet = new Set();
+    for (const [key, value] of Object.entries(checked)) {
+      if (value) ingredientSet.add(key);
+      else ingredientSet.delete(key);
+    }
+    console.log(ingredientSet);
+    setIngredientChosen(ingredientSet);
+  }, [checked]);
 
   return (
     <div>
@@ -60,7 +84,7 @@ const IngredientList = (props) => {
                 {/* htmlFor is what we need in react to connect the label to the input */}
                 <input
                   id={element.ingredient_id}
-                  name='test'
+                  name={element.ingredient_name}
                   type='checkbox'
                   checked={checked[element.ingredient_name] || false}
                   onChange={handleChange}
@@ -71,17 +95,18 @@ const IngredientList = (props) => {
             );
           })}
         </div>
-        <button type='submit' onClick={handleClick}>
-          Submit
-        </button>
+        <button type='submit'>Submit</button>
+        {/* maybe replace the above button with the button below? (tap to generate recipe) */}
       </form>
 
       <h5>
         This container might populate automatically as ingredients are selected
+        {listOfIngredientsChosen.length > 0 &&
+          listOfIngredientsChosen.map((element) => {
+            return <li>{element}</li>;
+          })}
       </h5>
-      <button onClick={handleIngredientListClick}>
-        Tap to generate recipe!
-      </button>
+      <button>Tap to generate recipe!</button>
     </div>
   );
 };
