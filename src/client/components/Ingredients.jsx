4aa287900e52871c4c00335f73cdfb406fb4bf7a
Jose Andrew <Jandrew205@outlook.com>
2024-12-19 19:52:52 -0500

    added comments throughout

--- src/client/components/Ingredients.jsx
+++ src/client/components/Ingredients.jsx
@@ -7,7 +7,7 @@ const IngredientList = (props) => {
 
   // const fakeIngredients = ['apple', 'banana', 'milk']; // this will be deleted just a test
   const [checked, setChecked] = useState({}); //keeps track of all checked boxes
-  const [ingredientChosen, setIngredientChosen] = useState(new Set());
+  const [ingredientChosen, setIngredientChosen] = useState(new Set()); //the set that will hold the ingredients that are selected
   const [recipeList, setRecipeList] = useState(null);
 
   // console.log('ingredientList:', ingredientList); //just testing to see if ingredient list prints
@@ -15,22 +15,13 @@ const IngredientList = (props) => {
 
   //adds to the stateObject the ingredients that are checked off
   const handleChange = (event) => {
-    const { name, checked } = event.target;
-    // console.log('name of item checked:', name);
-    // console.log(checked);
+    const { name, checked } = event.target; //destructures the name and checked properties from element that was checked
     setChecked((previous) => ({
-      ...previous,
-      [name]: checked,
+      ...previous, //grabs previous state
+      [name]: checked, //updates only the current box that changes
     }));
   };
 
-  const mockRecipeList = [
-    {
-      recipe_name: 'Spaghetti Carbonara',
-      instructions:
-        'Cook pasta al dente, reserving water.\nCook guanciale (or substitute) until crispy.\nWhisk eggs, yolks, cheeses, and pepper.\nCombine pasta with guanciale fat.\nToss pasta with egg mixture, adding pasta water as needed.\nServe immediately with extra cheese.',
-    },
-  ];
   //this function is used for the form submission
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -43,27 +34,25 @@ const IngredientList = (props) => {
       },
       body: JSON.stringify([...ingredientChosen]), //sending ingredients chosen to the backend
     })
-      .then((res) => {
-        res.json();
-      })
+      .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setRecipeList(data);
       })
       .catch((err) =>
         console.error('Frontend to backend communication breakdown:', err)
       );
   };
 
-  // console.log(checked);
+  //adds ingredients to a set if true, otherwise takes them out
   useEffect(() => {
     const ingredientSet = new Set();
     for (const [key, value] of Object.entries(checked)) {
-      if (value) ingredientSet.add(key);
-      else ingredientSet.delete(key);
+      if (value)
+        ingredientSet.add(key); //if value is true(checked off) then add
+      else ingredientSet.delete(key); //otherwise delete ingredient from the set
     }
-    console.log(ingredientSet);
-    setIngredientChosen(ingredientSet);
-  }, [checked]);
+    setIngredientChosen(ingredientSet); //sets the ingredients chosen to the state
+  }, [checked]); //only updates when checked box is changed
 
   return (
     <div>
